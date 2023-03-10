59fb2ee2728ed7beb1f7876ce396315076348718
CASSIO <cassio.bjhones@gmail.com>
2023-03-09 21:34:07 -0300

    atualizacao_usando_return

--- src/main.java
+++ src/main.java
@@ -1,10 +1,18 @@
 public class main {
     public static void main(String[] args) throws Exception {
-       System.out.println("---CALCULO DE ÁREA---");
-       quadrilatero.area(25);
-       quadrilatero.area(2, 2, 2);
-       quadrilatero.area(2d, 2d);
-       quadrilatero.area(5f, 5f);;
-       System.out.println("");
+       System.out.println("\n---CALCULO DE ÁREA---\n");
+
+       double areaQuadrado = quadrilatero.area(5) ;
+       System.out.println("-Area do quadrado: " + areaQuadrado);
+
+       double areaRetangulo = quadrilatero.area(2d,3d);
+       System.out.println("-Area do Retangulo: " + areaRetangulo);
+
+       double areaTrapezio = quadrilatero.area(2, 3, 4) ;
+       System.out.println("-Area do Trapezio: " + areaTrapezio);
+
+       double areaLosango = quadrilatero.area(3f, 4f);
+       System.out.println("-Area do Losango: " + areaLosango + "\n");
+
     }
 }
